e919cb0b5063f9dca6e32efccc8b96aae672d973
Naveen Kumar Kaliannan <naveenkumar5892@gmail.com>
2017-04-20 08:08:50 +0100

    Update shellscript.sh

--- shellscript.sh
+++ shellscript.sh
@@ -1,4 +1,4 @@
 make clean
 make
-mpirun -np 8 ./main inputs/Silica_beta_cristobalite.dat output/data/densityandenergy.dat output/data/RDF.dat output/data/BAD.dat
+mpirun -np 64 ./main inputs/Silica_beta_cristobalite.dat output/data/densityandenergy.dat output/data/RDF.dat output/data/BAD.dat
 make -C output/gnuplot/
